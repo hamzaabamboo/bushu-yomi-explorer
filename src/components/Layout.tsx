ac65b142b817c53a7a3dceca57ab7f417ac99f76
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-11 01:57:28 +0700

    fix: unused lib

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { Container } from "@chakra-ui/layout";
-import { Stack } from "@chakra-ui/react";
+import { Container, Stack } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 export const Layout = ({children}: {children:ReactNode}) => {
