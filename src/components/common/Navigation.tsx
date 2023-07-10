60e7b3c78c6d3bb64805035ec7f6babf43068117
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-11 02:10:28 +0700

    feat: small changes

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { KANA_ROWS } from "@/data/gojuuon";
+import { getPath } from "@/utils/getPath";
 import { Box, HStack, Link } from "@chakra-ui/react";
 
 export const Navigation = () => {
     return <HStack　w="full" backgroundColor="white" position="sticky" top={0} py={2}>
-        {KANA_ROWS.split("").map((letter,index) => <Link href={`/browse/${index}`}　key={letter} shadow="md" textAlign="center" py="2" w="full" fontSize="xl"><Box >{letter}</Box></Link>)}
+        {KANA_ROWS.split("").map((letter,index) => <Link href={getPath(`/browse/${index}`)}　key={letter} shadow="md" textAlign="center" py="2" w="full" fontSize="xl"><Box >{letter}</Box></Link>)}
     </HStack>
 }
--- src/components/home/HomeIntro.tsx
+++ src/components/home/HomeIntro.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { getPath } from "@/utils/getPath";
 import { Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 export const HomeIntro = () => {
     return <Stack>
-        <Heading><Link href="/">Radical-Sound Relationship for Kanji</Link></Heading>
+        <Heading><Link href={getPath("/")}>Radical-Sound Relationship for Kanji</Link></Heading>
         <Text>Collection of radical and kanji that has relationship with certain onyomi sounds, data extracted from <Link href="https://blog.goo.ne.jp/ishiseiji/e/3180bacae127e35db289189ea5b95a4a">Here</Link></Text>
         <Text>Some glyph may not be properly displayed. Bugs are expected</Text>
     </Stack>
