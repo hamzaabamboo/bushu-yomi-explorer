6e32e3654cdf52d8db530378ac41332722c2ecb8
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-11 01:49:26 +0700

    feat: change to pnpm

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: ghPages ? "/muni-web" : process.env.NEXT_PUBLIC_BASE_URL || "",
+    basePath: ghPages ? "/bushu-yomi-explorer" : process.env.NEXT_PUBLIC_BASE_URL || "",
     output: 'export',
 }
 
